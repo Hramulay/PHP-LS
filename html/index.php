d3314cb52e653195334970fb43187580a69b1a98
Daniil Belinskiy <dinazyver@gmail.com>
2023-04-08 23:46:21 +0300

    dz4

---
+++ html/index.php
@@ -0,0 +1,22 @@
+<?php
+require_once '../vendor/autoload.php';
+
+// Create the Transport
+$transport = (new Swift_SmtpTransport('smtp.mail.ru', 465, 'ssl'))
+    ->setUsername('ee.da.83@mail.ru')
+    ->setPassword('kJ73sa6iu$/58(S')
+;
+
+// Create the Mailer using your created Transport
+$mailer = new Swift_Mailer($transport);
+
+// Create a message
+$message = (new Swift_Message('Wonderful Subject'))
+    ->setFrom(['ee.da.83@mail.ru' => '12345'])
+    ->setTo(['dinazyver@gmail.com'])
+    ->setBody('Here is the message itself')
+;
+
+// Send the message
+$result = $mailer->send($message);
+var_dump($result);
